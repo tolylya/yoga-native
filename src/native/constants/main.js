aca71396df5011d90c56649862d10c0052100927
Simonov Anatoliy <tolylya220@gmail.com>
2018-01-04 15:45:31 +0200

    Constant fixed

--- src/native/constants/main.js
+++ src/native/constants/main.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform } from 'react-native';
 
 
-export const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+export const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;;
 export const test = 'test';
